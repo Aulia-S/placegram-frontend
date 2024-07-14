6decea63611b2af871cd5a375ead6c91ddda259e
Aulia Sabri <auliasabri43@gmail.com>
2024-07-14 22:03:23 +0700

    prepare deployment

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,47 +1,42 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext } from 'react'
 
-import Card from '../../shared/components/UIElements/Card';
-import Button from '../../shared/components/FormElements/Button';
-import Modal from '../../shared/components/UIElements/Modal';
-import Map from '../../shared/components/UIElements/Map';
-import ErrorModal from '../../shared/components/UIElements/ErrorModal';
-import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
-import { AuthContext } from '../../shared/context/auth-context';
-import { useHttpClient } from '../../shared/hooks/http-hook';
-import './PlaceItem.css';
+import Card from '../../shared/components/UIElements/Card'
+import Button from '../../shared/components/FormElements/Button'
+import Modal from '../../shared/components/UIElements/Modal'
+import Map from '../../shared/components/UIElements/Map'
+import ErrorModal from '../../shared/components/UIElements/ErrorModal'
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
+import { AuthContext } from '../../shared/context/auth-context'
+import { useHttpClient } from '../../shared/hooks/http-hook'
+import './PlaceItem.css'
 
-const PlaceItem = props => {
-  const { isLoading, error, sendRequest, clearError } = useHttpClient();
-  const auth = useContext(AuthContext);
-  const [showMap, setShowMap] = useState(false);
-  const [showConfirmModal, setShowConfirmModal] = useState(false);
+const PlaceItem = (props) => {
+  const { isLoading, error, sendRequest, clearError } = useHttpClient()
+  const auth = useContext(AuthContext)
+  const [showMap, setShowMap] = useState(false)
+  const [showConfirmModal, setShowConfirmModal] = useState(false)
 
-  const openMapHandler = () => setShowMap(true);
+  const openMapHandler = () => setShowMap(true)
 
-  const closeMapHandler = () => setShowMap(false);
+  const closeMapHandler = () => setShowMap(false)
 
   const showDeleteWarningHandler = () => {
-    setShowConfirmModal(true);
-  };
+    setShowConfirmModal(true)
+  }
 
   const cancelDeleteHandler = () => {
-    setShowConfirmModal(false);
-  };
+    setShowConfirmModal(false)
+  }
 
   const confirmDeleteHandler = async () => {
-    setShowConfirmModal(false);
+    setShowConfirmModal(false)
     try {
-      await sendRequest(
-        `http://localhost:5000/api/places/${props.id}`,
-        'DELETE',
-        null,
-        {
-          Authorization: 'Bearer ' + auth.token
-        }
-      );
-      props.onDelete(props.id);
+      await sendRequest(`/api/places/${props.id}`, 'DELETE', null, {
+        Authorization: 'Bearer ' + auth.token,
+      })
+      props.onDelete(props.id)
     } catch (err) {}
-  };
+  }
 
   return (
     <React.Fragment>
@@ -50,19 +45,19 @@ const PlaceItem = props => {
         show={showMap}
         onCancel={closeMapHandler}
         header={props.address}
-        contentClass="place-item__modal-content"
-        footerClass="place-item__modal-actions"
+        contentClass='place-item__modal-content'
+        footerClass='place-item__modal-actions'
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
       >
-        <div className="map-container">
+        <div className='map-container'>
           <Map center={props.coordinates} zoom={16} />
         </div>
       </Modal>
       <Modal
         show={showConfirmModal}
         onCancel={cancelDeleteHandler}
-        header="Are you sure?"
-        footerClass="place-item__modal-actions"
+        header='Are you sure?'
+        footerClass='place-item__modal-actions'
         footer={
           <React.Fragment>
             <Button inverse onClick={cancelDeleteHandler}>
@@ -74,32 +69,24 @@ const PlaceItem = props => {
           </React.Fragment>
         }
       >
-        <p>
-          Do you want to proceed and delete this place? Please note that it
-          can't be undone thereafter.
-        </p>
+        <p>Do you want to proceed and delete this place? Please note that it can't be undone thereafter.</p>
       </Modal>
-      <li className="place-item">
-        <Card className="place-item__content">
+      <li className='place-item'>
+        <Card className='place-item__content'>
           {isLoading && <LoadingSpinner asOverlay />}
-          <div className="place-item__image">
-            <img
-              src={`http://localhost:5000/${props.image}`}
-              alt={props.title}
-            />
+          <div className='place-item__image'>
+            <img src={process.env.REACT_APP_BASE_URL + '/' + props.image} alt={props.title} />
           </div>
-          <div className="place-item__info">
+          <div className='place-item__info'>
             <h2>{props.title}</h2>
             <h3>{props.address}</h3>
             <p>{props.description}</p>
           </div>
-          <div className="place-item__actions">
+          <div className='place-item__actions'>
             <Button inverse onClick={openMapHandler}>
               VIEW ON MAP
             </Button>
-            {auth.userId === props.creatorId && (
-              <Button to={`/places/${props.id}`}>EDIT</Button>
-            )}
+            {auth.userId === props.creatorId && <Button to={`/places/${props.id}`}>EDIT</Button>}
 
             {auth.userId === props.creatorId && (
               <Button danger onClick={showDeleteWarningHandler}>
@@ -110,7 +97,7 @@ const PlaceItem = props => {
         </Card>
       </li>
     </React.Fragment>
-  );
-};
+  )
+}
 
-export default PlaceItem;
+export default PlaceItem
